3a83e72f1e8d9cb836ad3da6b05625b5e9772d3d
Yanick Wilisky <yanickw@gmail.com>
2022-08-06 16:11:01 -0700

    Re submit my assignment from a fresh new clone of the repo

--- KaseiCoinCrowdsale.sol
+++ KaseiCoinCrowdsale.sol
@@ -1,51 +1,54 @@
 pragma solidity ^0.5.0;
 
+//  Import `KaseiCoin`, `Crowdsale`, `MintedCrowdsale` contracts from the OpenZeppelin library and the local directory.
 import "./KaseiCoin.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/Crowdsale.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/emission/MintedCrowdsale.sol";
 
-
-// Have the KaseiCoinCrowdsale contract inherit the following OpenZeppelin:
-// * Crowdsale
-// * MintedCrowdsale
-contract KaseiCoinCrowdsale { // UPDATE THE CONTRACT SIGNATURE TO ADD INHERITANCE
+// KaseiCoinCrowdsale contract inherit the OpenZeppelin Crowdsale and MintedCrowdsale libraries.
+contract KaseiCoinCrowdsale is Crowdsale, MintedCrowdsale {
     
     // Provide parameters for all of the features of your crowdsale, such as the `rate`, `wallet` for fundraising, and `token`.
     constructor(
-        // YOUR CODE HERE!
-    ) public Crowdsale(rate, wallet, token) {
-        // constructor can stay empty
+        uint rate,
+        address payable wallet,
+        KaseiCoin token
+    )   
+        public 
+        Crowdsale(rate, wallet, token)
+    {
+
     }
 }
 
-/*
 contract KaseiCoinCrowdsaleDeployer {
-    // Create an `address public` variable called `kasei_token_address`.
-    // YOUR CODE HERE!
-    // Create an `address public` variable called `kasei_crowdsale_address`.
-    // YOUR CODE HERE!
+    // Set variables to store the addresses of `KaseiCoin` and `KasaiCoinCrowdsale`.
+    address public kasei_token_address;
+    address public kasei_crowdsale_address;
 
     // Add the constructor.
     constructor(
-       // YOUR CODE HERE!
+        string memory name,
+        string memory symbol,
+        address payable wallet
     ) public {
         // Create a new instance of the KaseiCoin contract.
-        // YOUR CODE HERE!
+        KaseiCoin token = new KaseiCoin(name, symbol, 0);
         
         // Assign the token contract’s address to the `kasei_token_address` variable.
-        // YOUR CODE HERE!
+        kasei_token_address = address(token);
 
         // Create a new instance of the `KaseiCoinCrowdsale` contract
-        // YOUR CODE HERE!
+        KaseiCoinCrowdsale kasei_crowdsale = new KaseiCoinCrowdsale(1, wallet, token);
             
         // Aassign the `KaseiCoinCrowdsale` contract’s address to the `kasei_crowdsale_address` variable.
-        // YOUR CODE HERE!
+        kasei_crowdsale_address = address(kasei_crowdsale);
 
         // Set the `KaseiCoinCrowdsale` contract as a minter
-        // YOUR CODE HERE!
+        token.addMinter(kasei_crowdsale_address);
+        
         
         // Have the `KaseiCoinCrowdsaleDeployer` renounce its minter role.
-        // YOUR CODE HERE!
+        token.renounceMinter();
     }
 }
-*/
---
+++ README.md
@@ -0,0 +1,54 @@
+# KASEI Coin
+
+KaseiCoin is a ERC-20 compliant fungible token that is minted using a Crowdsale contract from the OpenZeppelin Solidity library which allows people to convert their money to KaseiCoin.
+
+---
+
+## Requirements
+
+REMIX IDE version 1.3.4
+
+This application was writen in Solidity within the Remix IDE desktop application and compiled using version 0.5.0
+
+---
+
+## Getting Started
+
+---
+
+## Evaluation Evidence
+
+![setAccount](./Evaluation_Evidence/01_KaseiCoin_Compiled.png)
+
+![setAccount](./Evaluation_Evidence/02_KaseiCoinCrowdsale_Compiled.png)
+
+![setAccount](./Evaluation_Evidence/03_KaseiCoinCrowdsaleDeployer_Compiled.png)
+
+![setAccount](./Evaluation_Evidence/04_KaseiCoin_CrowdsaleDeployer_Deploy.png)
+
+![setAccount](./Evaluation_Evidence/05_KaseiCoin_Crowdsale_Deploy.png)
+
+
+---
+
+## Contributors
+
+This application originated from a Berkeley Bootcamp.
+
+For any inquieries, feedbacks or comments about this project please email me at  [yanickw@gmail.com](mailto:yanickw@gmail.com)
+
+I can also be reached on  [LinkedIn](https://www.linkedin.com/in/yanickwilisky/)  or  [Twitter](https://twitter.com/yanickwilisky).
+
+---
+
+## License
+
+MIT License
+
+Copyright (c) 2022 Yanick Wilisky
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
